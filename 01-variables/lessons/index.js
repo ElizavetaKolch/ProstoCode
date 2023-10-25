f3905a96870f6b83ba98acd9e860f08ce6aa84fd
Elizaveta Kolchugina <lizakolchugina@mail.ru>
2023-10-25 16:42:46 +0400

    My first commit

---
+++ 01-variables/lessons/index.js
@@ -0,0 +1,20 @@
+const name ="Elizaveta";
+var myAge = 20;
+let isOpen = true;
+let num = 12;
+let str = "Hi!";
+let cup = null;
+let hat = undefined;
+var dog ={};
+const str2= "Hello world!";
+console.log(name,myAge,isOpen,num,str,cup,hat,dog,str2);
+console.log(7>5)
+console.log("Hello"=="World")
+console.log(7=="World")
+console.log(false || false)
+let a=5;
+let b="Hello";
+console.log(a>b)
+
+
+
