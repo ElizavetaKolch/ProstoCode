506fb8d9a91e8ea09e0a08627c6c72563b824020
Elizaveta Kolchugina <lizakolchugina@mail.ru>
2023-10-27 20:14:30 +0400

    my 5 commit

--- 01-variables/lessons/index.js
+++ 01-variables/lessons/index.js
@@ -13,24 +13,12 @@ console.log(7>5)//true
 console.log("Hello"=="World")//false
 console.log(7=="World")//false
 console.log(false || false) //false
-let a=5
-let b="Hello"
-console.log(a > b)//false
-let num_2=5
-let bool=false
-console.log(num_2 <= bool)//false
-let str_2='hi'
-let bool_2=false
-console.log(str_2 && bool_2) //false
-let num_3=5
-let null_1=null
-console.log(num_3 || null_1) // true
-let null_2=null
-let und_2=undefined
-console.log(null_2 < und_2)//false
-let und=undefined
-let pravda=true
-console.log(und > pravda)//false
+console.log(name > myAge)//false
+console.log(num <= isOpen)//false
+console.log(str && isOpen) //false
+console.log(num||cup) // false
+console.log(cup< hat)//false
+console.log(hat > isOpen)//true
 
 
 
